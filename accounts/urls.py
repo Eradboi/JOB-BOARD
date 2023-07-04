8bcc844c6e0d66d0d9f12bb0e576fe9ff38575c6
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2023-07-04 23:57:33 +0100

    making sure ignored files are ignored

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('', display_user_name, name="user_name"),
-    path('signup/', signup_view, name="sign_up"),
-    path('complete-profile/', CompleteProfileView.as_view(), name="complete_profile"),
-]
--- jobs/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Upload
-
-class JobUploadForm(forms.ModelForm):
-    class Meta:
-        model = Upload
-        fields = ('Title', 'Description','Duration','Type')
-
